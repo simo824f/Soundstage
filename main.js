5cd6351b65084dfa630b4f8e843cc3c4bb58b915
Heinrich wolff <hcwolff94@gmail.com>
2022-03-17 20:42:23 +0100

    billede skift hvis man vil

--- main.js
+++ main.js
@@ -6,3 +6,20 @@ function cursor(e) {
     mouse.style.top = e.pageY + 'px';
     mouse.style.left = e.pageX + 'px';
 }
+
+// knapper
+
+
+// let btn1 = document.querySelector('#btn1')
+// let btn2 = document.querySelector('#btn2')
+
+// btn1.addEventListener('click', () =>{
+//     document.body.style.backgroundImage = "url(./img/paint.png)";
+// });
+// btn2.addEventListener('click', () =>{
+//     document.body.style.backgroundImage = "url(./img/bgg.jpg)";
+// });
+
+// function changeBackground() {
+//     document.body.style.backgroundImage = "url(./img/paint.png)";
+// }
